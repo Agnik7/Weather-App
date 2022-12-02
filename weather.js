643226eb804b78e1a821f63e3a8fa1bc2e9bfeb0
Agnik7 <77234005+Agnik7@users.noreply.github.com>
2022-12-02 20:23:45 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
  let weather = {
-        apiKey: "0c9664299581818626948eb4fb24b92d",
+        apiKey: /*use your own API Key*/,
         fetchWeather: function(city){
             fetch(
                 "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey
